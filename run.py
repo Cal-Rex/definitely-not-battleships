126b0eb6f2da6588b09581973561aa5c049e5f40
Cal-Rex <andy.gibb@healthcareskills.com>
2022-11-25 12:54:05 +0000

    add function to unpack returned tuple from choosing coords

--- run.py
+++ run.py
@@ -6,8 +6,7 @@ def pick_coords():
 
     print(column_guess)
     column_guess_converted = ord(column_guess) - 96
-    print(column_guess_converted)
-
+    print(f"\nYou Selected: {column_guess_converted}\n")
 
     row_guess = input("pick a numbered row between 1 - 5: ")
     while row_guess not in "12345":
@@ -15,35 +14,47 @@ def pick_coords():
         row_guess = input("pick a numbered row between 1 - 5: ")
 
     row_guess = int(row_guess)
-    print(row_guess)
-    #revisit thi function to zip 2 coordinates into a single dict
+    print(f"\nYou selected {row_guess}\n")
+    # 2 values used to target specific key:value pairs
+    return column_guess_converted, row_guess
 
 
-#convert these lists into single dictionary for coordinates to index through
-p_row_1 = ["1|", "~", "~", "~", "~", "~", "~", "~", "~"]
-p_row_2 = ["2|", "~", "~", "~", "~", "~", "~", "~", "~"]
-p_row_3 = ["3|", "~", "~", "~", "~", "~", "~", "~", "~"]
-p_row_4 = ["4|", "~", "~", "~", "~", "~", "~", "~", "~"]
-p_row_5 = ["5|", "~", "~", "~", "~", "~", "~", "~", "~"]
+def fire_torpedo():
+    """
+    chooses coordinates to strike
+    """
+    col, row = pick_coords()
+    print(col, row)
 
-c_row_1 = ["|1", "~", "~", "~", "~", "~", "~", "~", "~"]
-c_row_2 = ["|2", "~", "~", "~", "~", "~", "~", "~", "~"]
-c_row_3 = ["|3", "~", "~", "~", "~", "~", "~", "~", "~"]
-c_row_4 = ["|4", "~", "~", "~", "~", "~", "~", "~", "~"]
-c_row_5 = ["|5", "~", "~", "~", "~", "~", "~", "~", "~"]
+
+p_board = {
+    1: ["1|", "~", "~", "~", "~", "~", "~", "~", "~"],
+    2: ["2|", "~", "~", "~", "~", "~", "~", "~", "~"],
+    3: ["3|", "~", "~", "~", "~", "~", "~", "~", "~"],
+    4: ["4|", "~", "~", "~", "~", "~", "~", "~", "~"],
+    5: ["5|", "~", "~", "~", "~", "~", "~", "~", "~"]
+    }
+
+c_board = {
+    1: ["|1", "~", "~", "~", "~", "~", "~", "~", "~"],
+    2: ["|2", "~", "~", "~", "~", "~", "~", "~", "~"],
+    3: ["|3", "~", "~", "~", "~", "~", "~", "~", "~"],
+    4: ["|4", "~", "~", "~", "~", "~", "~", "~", "~"],
+    5: ["|5", "~", "~", "~", "~", "~", "~", "~", "~"]
+    }
 
 
 def create_board():
     print("+| A B C D E F G H | A B C D E F G H |+")
     print("-|-----------------|-----------------|-")
-    print(" ".join(p_row_1), "|", " ".join(c_row_1[1:]), c_row_1[0])
-    print(" ".join(p_row_2), "|", " ".join(c_row_2[1:]), c_row_2[0])
-    print(" ".join(p_row_3), "|", " ".join(c_row_3[1:]), c_row_3[0])
-    print(" ".join(p_row_4), "|", " ".join(c_row_4[1:]), c_row_4[0])
-    print(" ".join(p_row_5), "|", " ".join(c_row_5[1:]), c_row_5[0])
+    print(" ".join(p_board[1]), "|", " ".join(c_board[1][1:]), c_board[1][0])
+    print(" ".join(p_board[2]), "|", " ".join(c_board[2][1:]), c_board[2][0])
+    print(" ".join(p_board[3]), "|", " ".join(c_board[3][1:]), c_board[3][0])
+    print(" ".join(p_board[4]), "|", " ".join(c_board[4][1:]), c_board[4][0])
+    print(" ".join(p_board[5]), "|", " ".join(c_board[5][1:]), c_board[5][0])
     print("-|-----------------|-----------------|-")
     print("+| A B C D E F G H | A B C D E F G H |+\n")
 
 
 create_board()
-pick_coords()
+fire_torpedo()
